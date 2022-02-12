6f1ae1fa9a161c33c60f858f92ea3a656db3e42e
Tasnim Nahid <nnnahidt@gmail.com>
2022-02-12 20:59:23 +0600

    initialize firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBOL6W-G2uMjmyZLxz7Swx-1AL3tkHTxI4",
+  authDomain: "e-vozon-scic-task.firebaseapp.com",
+  projectId: "e-vozon-scic-task",
+  storageBucket: "e-vozon-scic-task.appspot.com",
+  messagingSenderId: "105406517923",
+  appId: "1:105406517923:web:84e0dae8f56b015d82e516"
+};
+
+export default firebaseConfig;
