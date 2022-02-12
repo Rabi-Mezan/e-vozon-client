16e2aac46f381de62be39ea699953d024fdefe0b
Tasnim Nahid <nnnahidt@gmail.com>
2022-02-12 21:28:08 +0600

    install react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from './Login/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <button>Google Sign In</button>
+        </div>
+    );
+};
+
+export default Login;
